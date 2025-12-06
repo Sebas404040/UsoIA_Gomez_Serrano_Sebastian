7645807f43e387f693a8cf3f744c8e29570fb3ed
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-12-05 22:39:22 -0500

    feat: :sparkles: New functionalities
    Functionalities created successfully for the CRUD
    
    none

--- dia_2/ejercicio_1/script.js
+++ dia_2/ejercicio_1/script.js
@@ -27,7 +27,53 @@ async function fetchAPI(endpoint, method = 'GET', body = null) {
         const data = await response.json();
         return data;
     } catch (error) {
-       console.log("Error al cargar los datos", error); 
+        console.log("Error al cargar los datos", error);
     }
 }
 
+async function agregarTarea(nombreTarea) {
+    try {
+        const nuevaTarea = {
+            name: nombreTarea,
+            completed: false
+        }
+
+        const tareaCreada = await fetchAPI('tasks', 'POST', nuevaTarea);
+        return tareaCreada;
+    } catch (error) {
+        console.log("Error al agregar la tarea", error);
+    }
+}
+
+async function mostrarTareas() {
+    try {
+        const tareas = await fetchAPI('tareas');
+        tareasTotal = tareas;
+        return tareasTotal;
+    } catch (error) {
+        console.log("Error al mostrar las tareas", error);
+    }
+}
+
+async function editarTarea(id, estado) {
+    try {
+        const datos = {
+            isCompleted: !estado
+        }
+        const tareaEditada = await fetchAPI(`tasks/${id}`, 'PUT', datos);
+        const index = tareasTotal.findIndex(tarea => tarea.id === id);
+        if (index !== -1) {
+            tareasTotal[index] = tareaEditada;
+        }
+    } catch (error) {
+        console.log("Error al editar la tarea", error);
+    }
+}
+
+async function borrarTarea(id) {
+    const respuesta = await fetchAPI(`tasks/${id}`, "DELETE");
+
+    if (respuesta) {
+        tareasTotal = tareasTotal.filter(tarea => tarea.id !== id);
+    }
+}
